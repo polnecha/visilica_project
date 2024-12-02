4ca1a5e8797fc28e65f6185dd242cf434bcccd44
embloyd <muraianastasia@yandex.ru>
2024-12-02 16:42:32 +0500

    Функция для отображения текущего состояния игры

--- main.py
+++ main.py
@@ -15,3 +15,9 @@ attempts_left = max_attempts
 
 # Множество для хранения уже введенных букв
 guessed_letters = set()
+
+def display_game():
+    print(hangman_images[max_attempts - attempts_left])  # Показываем картинку в зависимости от оставшихся попыток
+    print('Текущее слово: ' + ' '.join(guessed_word))
+    print(f'Оставшиеся попытки: {attempts_left}')
+    print(f'Использованные буквы: {", ".join(sorted(guessed_letters))}')
