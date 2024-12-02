3e36a308c8dd7d31220f175cb5555d97b810f41a
Selectel Git User <git-user@selectel.ru>
2024-12-02 16:26:22 +0500

    Подготовка данных и начальная настройка

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-print('eeeea')#fjjdоврыоыр
-p = 1
+import random
+
+# Список слов для игры
+words = ['python', 'java', 'hangman', 'programming', 'development']
+
+# Выбираем случайное слово из списка
+word_to_guess = random.choice(words)
+
+# Список для отображения угаданного слова с подставленными _ вместо букв
+guessed_word = ['_'] * len(word_to_guess)
+
+# Количество попыток (ошибок)
+max_attempts = 6
+attempts_left = max_attempts
+
+# Множество для хранения уже введенных букв
+guessed_letters = set()
